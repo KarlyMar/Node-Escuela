23edfd1e1c584541826974d3e6920c3f018e4570
KarlyMar <kaesmarmolejour@ittepic.edu.mx>
2024-05-23 08:50:57 -0700

    ya funcionan las querys ya estan probadas

--- src/controllers/alumno.js
+++ src/controllers/alumno.js
@@ -1,4 +1,5 @@
 const alumno = require('../models/alumno');
+const Grupo = require('../models/grupo');
 
 
 exports.obtenerAlumnos = async (req, res) => {
@@ -69,192 +70,191 @@ exports.eliminarAlumno = async (req, res) => {
 };
 
 
-  exports.q3 = async (req, res) => {
+exports.q1 = async (req, res) => {
     const { nctrl } = req.params;
     try {
-      const resultado = await Alumno.aggregate([
-        { $match: { nctrl: nctrl } },
-        { $unwind: "$expediente_academico.grupos_cursados" },
-        {
-          $lookup: {
-            from: "grupos",
-            localField: "expediente_academico.grupos_cursados.grupo",
-            foreignField: "id",
-            as: "expediente_academico.grupos_cursados.grupo",
-          },
-        },
-        { $unwind: "$expediente_academico.grupos_cursados.grupo" },
-        {
-          $group: {
-            _id: "$nctrl",
-            alumno: { $first: "$datos" },
-            materias: {
-              $push: {
-                calificacion: "$expediente_academico.grupos_cursados.calificacion",
-                materia: "$expediente_academico.grupos_cursados.grupo.materia",
-              },
+        const resultado = await alumno.aggregate([
+            { $match: { nctrl: nctrl } },
+            { $unwind: "$expediente_academico.grupos_cursados" },
+            {
+                $lookup: {
+                    from: "grupos",
+                    localField: "expediente_academico.grupos_cursados.grupo",
+                    foreignField: "id",
+                    as: "expediente_academico.grupos_cursados.grupo",
+                },
             },
-          },
-        },
-      ]);
-      res.status(200).send(resultado);
+            { $unwind: "$expediente_academico.grupos_cursados.grupo" },
+            {
+                $group: {
+                    _id: "$nctrl",
+                    alumno: { $first: "$datos" },
+                    materias: {
+                        $push: "$expediente_academico.grupos_cursados.grupo.materia",
+                    },
+                },
+            },
+        ]);
+        res.status(200).send(resultado);
     } catch (error) {
-      res.status(500).send({ error: `Error al obtener las calificaciones del alumno con nctrl: ${nctrl}`, detalle: error.message });
+        res.status(500).send({ error: `Error al obtener las materias cursadas del alumno con nctrl: ${nctrl}`, detalle: error.message });
     }
-  };
-
-  exports.q1 = async (req, res) => {
+};
+exports.q2 = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const resultado = await Grupo.findOne({ id }, 'materia alumnos');
+        if (!resultado) {
+            return res.status(404).send({ error: `El grupo con el id ${id} no fue encontrado` });
+        }
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(500).send({ error: `Error al obtener los alumnos del grupo con id: ${id} que cursan la materia`, detalle: error.message });
+    }
+};
+exports.q3 = async (req, res) => {
     const { nctrl } = req.params;
     try {
-      const resultado = await Alumno.aggregate([
-        { $match: { nctrl: nctrl } },
-        { $unwind: "$expediente_academico.grupos_cursados" },
-        {
-          $lookup: {
-            from: "grupos",
-            localField: "expediente_academico.grupos_cursados.grupo",
-            foreignField: "id",
-            as: "expediente_academico.grupos_cursados.grupo",
-          },
-        },
-        { $unwind: "$expediente_academico.grupos_cursados.grupo" },
-        {
-          $group: {
-            _id: "$nctrl",
-            alumno: { $first: "$datos" },
-            materias: {
-              $push: "$expediente_academico.grupos_cursados.grupo.materia",
+        const resultado = await alumno.aggregate([
+            { $match: { nctrl: nctrl } },
+            { $unwind: "$expediente_academico.grupos_cursados" },
+            {
+                $lookup: {
+                    from: "grupos",
+                    localField: "expediente_academico.grupos_cursados.grupo",
+                    foreignField: "id",
+                    as: "expediente_academico.grupos_cursados.grupo",
+                },
             },
-          },
-        },
-      ]);
-      res.status(200).send(resultado);
+            { $unwind: "$expediente_academico.grupos_cursados.grupo" },
+            {
+                $group: {
+                    _id: "$nctrl",
+                    alumno: { $first: "$datos" },
+                    materias: {
+                        $push: {
+                            calificacion: "$expediente_academico.grupos_cursados.calificacion",
+                            materia: "$expediente_academico.grupos_cursados.grupo.materia",
+                        },
+                    },
+                },
+            },
+        ]);
+        res.status(200).send(resultado);
     } catch (error) {
-      res.status(500).send({ error: `Error al obtener las materias cursadas del alumno con nctrl: ${nctrl}`, detalle: error.message });
+        res.status(500).send({ error: `Error al obtener las calificaciones del alumno con nctrl: ${nctrl}`, detalle: error.message });
     }
-  };
-  
-  exports.q7 = async (req, res) => {
+};
+exports.q7 = async (req, res) => {
     const { nctrl } = req.params;
     let { hora } = req.params;
     hora = parseInt(hora);
     try {
-      const resultado = await Alumno.aggregate([
-        { $match: { nctrl: nctrl } },
-        {
-          $lookup: {
-            from: "grupos",
-            localField: "expediente_academico.grupos_cursados.grupo",
-            foreignField: "id",
-            as: "expediente_academico.grupos_cursados.grupo",
-          },
-        },
-        { $unwind: '$expediente_academico.grupos_cursados.grupo' },
-        { $match: { 'expediente_academico.grupos_cursados.grupo.horario': hora } },
-        {
-          $group: {
-            '_id': '$nctrl',
-            'alumno': { $first: '$datos' },
-            'materias': {
-              $push: {
-                'horario': '$expediente_academico.grupos_cursados.grupo.horario',
-                'materia': '$expediente_academico.grupos_cursados.grupo.materia',
-              }
+        const resultado = await alumno.aggregate([
+            { $match: { nctrl: nctrl } },
+            {
+                $lookup: {
+                    from: "grupos",
+                    localField: "expediente_academico.grupos_cursados.grupo",
+                    foreignField: "id",
+                    as: "expediente_academico.grupos_cursados.grupo",
+                },
+            },
+            { $unwind: '$expediente_academico.grupos_cursados.grupo' },
+            { $match: { 'expediente_academico.grupos_cursados.grupo.horario': hora } },
+            {
+                $group: {
+                    '_id': '$nctrl',
+                    'alumno': { $first: '$datos' },
+                    'materias': {
+                        $push: {
+                            'horario': '$expediente_academico.grupos_cursados.grupo.horario',
+                            'materia': '$expediente_academico.grupos_cursados.grupo.materia',
+                        }
+                    }
+                }
             }
-          }
-        }
-      ]);
-      res.status(200).send(resultado);
+        ]);
+        res.status(200).send(resultado);
     } catch (error) {
-      res.status(500).send({ error: `Error al obtener las materias del alumno con nctrl: ${nctrl} a la hora: ${hora}`, detalle: error.message });
+        res.status(500).send({ error: `Error al obtener las materias del alumno con nctrl: ${nctrl} a la hora: ${hora}`, detalle: error.message });
     }
-  };
-  
-  exports.q8 = async (req, res) => {
+};
+
+exports.q8 = async (req, res) => {
     const { nctrl } = req.params;
     try {
-      const resultado = await Alumno.aggregate([
-        { $match: { nctrl: nctrl } },
-        {
-          $lookup: {
-            from: "grupos",
-            localField: "expediente_academico.grupos_cursados.grupo",
-            foreignField: "id",
-            as: "expediente_academico.grupos_cursados.grupo",
-          },
-        },
-        {
-          $project: {
-            _id: '$nctrl',
-            alumno: '$datos',
-            materias: '$expediente_academico.grupos_cursados.grupo.materia',
-            reticula: '$expediente_academico.reticula'
-          }
-        },
-        {
-          $addFields: {
-            // Extraemos los IDs de las materias cursadas
-            materiasIds: {
-              $map: {
-                input: '$materias',
-                as: 'materia',
-                in: '$$materia.id'
-              }
+        const resultado = await alumno.aggregate([
+            { $match: { nctrl: nctrl } },
+            {
+                $lookup: {
+                    from: "grupos",
+                    localField: "expediente_academico.grupos_cursados.grupo",
+                    foreignField: "id",
+                    as: "expediente_academico.grupos_cursados.grupo",
+                },
             },
-            // Extraemos los IDs de la reticula
-            reticulaIds: {
-              $map: {
-                input: '$reticula',
-                as: 'materia',
-                in: '$$materia.id'
-              }
+            {
+                $project: {
+                    _id: '$nctrl',
+                    alumno: '$datos',
+                    materias: '$expediente_academico.grupos_cursados.grupo.materia',
+                    reticula: '$expediente_academico.reticula'
+                }
+            },
+            {
+                $addFields: {
+                    // Extraemos los IDs de las materias cursadas
+                    materiasIds: {
+                        $map: {
+                            input: '$materias',
+                            as: 'materia',
+                            in: '$$materia.id'
+                        }
+                    },
+                    // Extraemos los IDs de la reticula
+                    reticulaIds: {
+                        $map: {
+                            input: '$reticula',
+                            as: 'materia',
+                            in: '$$materia.id'
+                        }
+                    }
+                }
+            },
+            {
+                $addFields: {
+                    // Obtenemos los IDs de las materias que están en reticula pero no en materias cursadas
+                    materiasPorCursarIds: {
+                        $setDifference: ['$reticulaIds', '$materiasIds']
+                    }
+                }
+            },
+            {
+                $addFields: {
+                    // Filtramos las materias de la reticula para obtener los subdocumentos completos de las materias por cursar
+                    materiasPorCursar: {
+                        $filter: {
+                            input: '$reticula',
+                            as: 'materia',
+                            cond: { $in: ['$$materia.id', '$materiasPorCursarIds'] }
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    alumno: 1,
+                    materiasPorCursar: 1
+                }
             }
-          }
-        },
-        {
-          $addFields: {
-            // Obtenemos los IDs de las materias que están en reticula pero no en materias cursadas
-            materiasPorCursarIds: {
-              $setDifference: ['$reticulaIds', '$materiasIds']
-            }
-          }
-        },
-        {
-          $addFields: {
-            // Filtramos las materias de la reticula para obtener los subdocumentos completos de las materias por cursar
-            materiasPorCursar: {
-              $filter: {
-                input: '$reticula',
-                as: 'materia',
-                cond: { $in: ['$$materia.id', '$materiasPorCursarIds'] }
-              }
-            }
-          }
-        },
-        {
-          $project: {
-            _id: 1,
-            alumno: 1,
-            materiasPorCursar: 1
-          }
-        }
-      ]);
-      res.status(200).send(resultado);
+        ]);
+        res.status(200).send(resultado);
     } catch (error) {
-      res.status(500).send({ error: `Error al obtener las materias por cursar del alumno con nctrl: ${nctrl}`, detalle: error.message });
+        res.status(500).send({ error: `Error al obtener las materias por cursar del alumno con nctrl: ${nctrl}`, detalle: error.message });
     }
-  };
-  
-  exports.q2 = async (req, res) => {
-    const { id } = req.params;
-    try {
-      const resultado = await Grupo.findOne({ id }, 'materia alumnos');
-      if (!resultado) {
-        return res.status(404).send({ error: `El grupo con el id ${id} no fue encontrado` });
-      }
-      res.status(200).send(resultado);
-    } catch (error) {
-      res.status(500).send({ error: `Error al obtener los alumnos del grupo con id: ${id} que cursan la materia`, detalle: error.message });
-    }
-  };
+};
+
+
 
